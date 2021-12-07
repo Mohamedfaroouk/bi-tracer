e3fd12a09270ce15a17aa1eca4d196a0ff32479a
ssaye <ssayedaashrf@gmail.com>
2021-12-07 18:11:18 +0200

    auth design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bi_tracer/screens/login.dart';
+import 'package:bi_tracer/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,17 +12,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Bi-Tracer',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+              color: Colors.transparent,
+              titleTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500),
+              elevation: 0,
+              iconTheme: IconThemeData(color: Colors.black)),
+          scaffoldBackgroundColor: backgroundColor,
+          primarySwatch: mainColor
+
       ),
       home: LogIn(),
 
---
+++ lib/shared/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const mainColor = Colors.deepPurple;
+const backgroundColor = Color(0xFFEBE8F0);
